d34b480359e55b94c91a765ba076e76a3d3e5e49
sushmita <ksushmita307@gmail.com>
2021-09-30 08:31:23 +0530

    Addition

--- src/com/bridgelab/AddressBookMain.java
+++ src/com/bridgelab/AddressBookMain.java
@@ -1,8 +1,80 @@
 package com.bridgelab;
+class contactInfo {
+    private String firstName, lastName, address, city, state, email;
+    private long zip, phoneNumber;
+
+    /* Generating setters and getters for class */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(long zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+}
 
 public class AddressBookMain {
+
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book program ");
 
     }
+
 }
